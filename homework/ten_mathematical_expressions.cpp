1a818a29d90b757380a2ea99df84f7ca9be76c70
Giselle <gisellevi002@gmail.com>
2025-09-02 11:25:38 -0400

    first commit

---
+++ homework/ten_mathematical_expressions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int n = 24;
+int m = 2;
+int o = 9;
+int p = 8;
+cout << "The sum  of " << n << " and " << m << " is ";
+cout << n + m << "." << endl;
+cout << "The difference of " << n << " and " << m << " is ";
+cout << n - m << "." << endl;
+cout << "The product of " << n << " and " << m << " is ";
+cout << n * m << "." << endl;
+cout << "The quotient of " << n << " divided by " << m << " is ";
+cout << n / m << "." << endl;
+cout << "The remainder of " << n << " divided by " << o << " is ";
+cout << n % o << "." << endl;
+cout << " The result of " << o << " minus " << m << " multiplied by " << n <<" is ";
+cout << ( o - m ) * n << "." << endl;
+cout << " The result of " << n << " divided by " << m << " subracted by " <<o << " is ";
+cout << (n - m) - o << "." << endl;
+cout << " The result of " << n << " divided by " << p << " multiplied by " << m << " is ";
+cout << (n / p) * m << "." << endl;
+cout << " The result of " << o << " minus " << m << " plus " << p << " is ";
+cout << (o - m) + p << "." << endl;
+cout << " The result of " << p << " times " << m << " plus " << n << " is ";
+cout << (p * m) + n << "." << endl;
+return 0;
+}
+
+
